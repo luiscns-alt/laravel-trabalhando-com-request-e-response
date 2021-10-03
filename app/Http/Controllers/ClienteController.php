5a6519869cc66c14f386117a734d8bd81e5656f0
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-03 11:00:21 -0300

    [features] Métodos query, has e filled

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -24,16 +24,37 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        dd(
-            // $request->input('nome', 'nome não definido'),
-            // $request->input('idade', 'idade não definida'),
-            // $request->input('em')
-            // $request->nome,
-            // $request->idade,
-            //$request->all(),
-            //$request->except('_token')
-            $request->only(['nome', 'idade'])
-        );
+
+        // dd(
+        // $request->input('nome', 'nome não definido'),
+        // $request->input('idade', 'idade não definida'),
+        // $request->input('em')
+        // $request->nome,
+        // $request->idade,
+        // $request->all(),
+        // $request->except('_token')
+        // $request->only(['nome', 'idade'])
+
+        /**
+         * Métodos query, has e filled
+         */
+        //Query pega os dados passados na url
+        //     //$request->query('nome', 'não envio nada')
+        // );
+        /**
+         * Método Has verifica se existir um campo
+         */
+        //     if ($request->has('nome')) {
+        //         dd('existe');
+        //     }
+        //     dd('não existe');
+        // }
+        /**
+         * Método filled vericar se o campo foi preenchido
+         */
+        if ($request->filled('nome', 'idade')) {
+            dd('campo nome ou idade foi preenchido');
+        }
+        dd('campo nome ou idade não foi preenchido');
     }
 }
